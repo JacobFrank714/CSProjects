33244e10e16734cd71cf4a72c107bab676b52453
Jacob Frank <jacobfrank@gmail.com>
2021-10-08 13:43:31 -0500

    Saving all my sorting algos

---
+++ SortingAlgos/Bubblesort.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+int counter = 0;
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+    counter += 1;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)    
+     
+    // Last i elements are already in place
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+}
